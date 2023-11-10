edd7f1273544d32428cff6344d12c4cdc8b30bd1
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2023-11-10 16:56:24 -0300

    implementacao da matriz de arraylist

--- CampoMinado.java
+++ CampoMinado.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class CampoMinado {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int tamanhoX = 10;
+        int tamanhoY = 15;
+        ArrayList<ArrayList<Integer>> mapa = new ArrayList<ArrayList<Integer>>(tamanhoX);
+        for (int i = 0; i < tamanhoX; i++){
+            mapa.add(new ArrayList<Integer>(tamanhoY));
+        }
+        for (int i = 0; i < tamanhoX; i++ ){
+            for (int j = 0; j < tamanhoY; j++){
+                mapa.get(i).add(i*j);
+            }
+        }
+        for (int i = 0; i < tamanhoX; i++ ){
+            for (int j = 0; j < tamanhoY; j++){
+                System.out.print("\t" + mapa.get(i).get(j));
+            }
+        System.out.println("");
+        }
+
     }
 }
