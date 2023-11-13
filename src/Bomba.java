077613d3214d1f627849c9977b411170c9393540
GabrielZoega <gabriel.z.fernandes@ufv.br>
2023-11-13 13:18:24 -0300

    Implementando a Classe Bloco

--- src/Bomba.java
+++ src/Bomba.java
@@ -1,7 +1,15 @@
 // TODO: Verificar possibilidade de criacao da funcao de fazer som de explosao
 
-public class Bomba {
+public class Bomba extends Bloco{
     
+    //public void revelaQPerdeu(){
+    //    
+    //}
+
+    @Override
+    public boolean isBomba(){
+        return true;
+    }
 }
 
 
