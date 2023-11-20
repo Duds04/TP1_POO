d11570d7957cd6ce723f6123e748860c89ac6243
João Bradachi <joao.bradachi@ufv.br>
2023-11-20 09:08:47 -0300

    geramapa

--- src/Mapa.java
+++ src/Mapa.java
@@ -20,12 +20,38 @@ public class Mapa {
         this.totCelulas = (linhas*colunas) - totalBombas;
     }
     public int geraMapa(){
-        // para cada elemento do arraylist mais interno, deve-se criar um novo array list dentro dele. Segue pseudo
-        // for M in matriz MxN:
-        //      mapa.add(new ArrayList(N))
-        // 
-        return 0;
+        // separei linhas e colunas no caso de querermos fazer um campo minado de outra forma, não necessáriamente quadrado
+        int i = 0, j= 0;
+        double posX, posY;
+        if(dificuldade.toUpperCase() == "FACIL"){
+            this.linhas = 8;
+            this.colunas = 8;
+            this.totalBombas = 10;
+            //this.mapa = new Bloco[linhas][colunas];
+        }
+        else if(dificuldade.toUpperCase() == "MEDIO"){
+            this.linhas = 14;
+            this.colunas = 14;
+            this.totalBombas = 40;
+            //this.mapa = new Bloco[linhas][colunas];
+        }
+        else if(dificuldade.toUpperCase() == "DIFICIL"){
+            this.linhas = 20;
+            this.colunas = 20;
+            this.totalBombas = 99;
+            //this.mapa = new Bloco[linhas][colunas];
+        }
+        while (i<this.totalBombas) {
+            posY = Math.floor(Math.random()*this.linhas);
+            posX = Math.floor(Math.random()*this.colunas);
+            //if( não tem bomba aqui){
+            //  cria bomba aqui 
+            // i++    
+            //}
+        }
+        return 1;
     }
+    
     public int completaCampo(){
         return 0;
     }
