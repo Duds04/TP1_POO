500ed8d343700834d0a01176b1a5714f3d952b3e
GabrielZoega <gabriel.z.fernandes@gmail.com>
2023-12-10 17:44:40 -0300

    Colorindo os botões, menu e backgrounds

--- src/App.java
+++ src/App.java
@@ -13,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
@@ -72,8 +73,9 @@ public class App extends Application{
                 mapa = new Mapa("FACIL");
                 // Add buttons to the grid pane
                 iniciaGrid();
-    
-                Scene scene = new Scene(grid, 300, 200);
+                
+                grid.setStyle("-fx-background-color: rgb(" + 150 + "," + 150 + ", " + 150 + ");");
+                Scene scene = new Scene(grid, 620, 450);
                 stage.setScene(scene);
                 stage.setTitle("Campo Minado");
                 stage.show();
@@ -86,8 +88,9 @@ public class App extends Application{
                 mapa = new Mapa("MEDIO");
                 // Add buttons to the grid pane
                 iniciaGrid();
-    
-                Scene scene = new Scene(grid, 400, 400);
+                
+                grid.setStyle("-fx-background-color: gray;");
+                Scene scene = new Scene(grid, 1000, 900);
                 stage.setScene(scene);
                 stage.setTitle("Campo Minado");
                 stage.show();
@@ -101,7 +104,8 @@ public class App extends Application{
                 // Add buttons to the grid pane
                 iniciaGrid();
     
-                Scene scene = new Scene(grid, 500, 500);
+                grid.setStyle("-fx-background-color: gray;");
+                Scene scene = new Scene(grid, 1700, 1000);
                 stage.setScene(scene);
                 stage.setTitle("Campo Minado");
                 stage.show();
@@ -117,9 +121,12 @@ public class App extends Application{
             caixaBotoes.setAlignment(Pos.CENTER);
             caixaBotoes.getChildren().addAll(botaoFacil, botaoMedio, botaoDificil);
 
+            root.setStyle("-fx-background-color: lightgray;");
             root.getChildren().add(caixaTexto);
             root.getChildren().add(caixaBotoes);
             Scene scene = new Scene(root, 400, 400);
+            Image icone = new Image("bomb1.png");
+            stage.getIcons().add(icone);
             stage.setScene(scene);
             stage.setTitle("Campo Minado");
             stage.show();
@@ -128,9 +135,14 @@ public class App extends Application{
     
     private void addPane(int colIndex, int rowIndex) {
         Button pane = new Button(" ");
-        pane.setPrefSize(30, 30);
+        pane.setStyle("-fx-background-color: gray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+        pane.setPrefSize(80, 80);
         pane.setOnMouseClicked(e -> {
             System.out.printf("Mouse enetered cell [%d, %d]%n", colIndex, rowIndex);
+
+            if (mapa.mapa[rowIndex][colIndex].isBandeira() && e.getButton() == MouseButton.PRIMARY){
+                return;
+            }
 
             if (mapa.mapa[rowIndex][colIndex].isBomba() && (e.getButton() == MouseButton.PRIMARY)){
                 Stage stagePerdeu = new Stage();
@@ -160,6 +172,8 @@ public class App extends Application{
                 Scene scenePerdeu = new Scene(root, 400, 300);
                 stagePerdeu.setScene(scenePerdeu);
                 stagePerdeu.setTitle("Voce perdeu");
+                Image icone = new Image("bomb1.png");
+                stagePerdeu.getIcons().add(icone);
                 // Show the new stage
                 Stage stageAtual = (Stage) pane.getScene().getWindow();
                 stageAtual.close();
@@ -193,6 +207,8 @@ public class App extends Application{
                 Scene sceneGanhou = new Scene(rootGanhou, 400, 300);
                 stageGanhou.setScene(sceneGanhou);
                 stageGanhou.setTitle("Voce ganhou");
+                Image icone = new Image("bomb1.png");
+                stageGanhou.getIcons().add(icone);
                 // Show the new stage
                 Stage stageAtual = (Stage) pane.getScene().getWindow();
                 stageAtual.close();
@@ -200,9 +216,6 @@ public class App extends Application{
             }
 
             if (e.getButton() == MouseButton.SECONDARY){
-                //if (mapa.mapa[rowIndex][colIndex].isBomba()){
-                //    return;
-                //}
                 System.out.println("mouse direito");
                 mapa.modificaBadeira(rowIndex, colIndex);
                 for (Node node : grid.getChildren()) {
@@ -211,11 +224,17 @@ public class App extends Application{
                         int j = GridPane.getColumnIndex(node);
                         // if(mapa.mapa[i][j].isBomba()) ((Button) node).setText("X"); // vai mostrar os Xs
                         if (mapa.mapa[i][j].isBandeira() && mapa.mapa[i][j].isRevelado() == false){
-                            ((Button) node).setText("B");
+
+                            Image imagemBandeira = new Image("imagemBandeira.png");
+                            ImageView viewBandeira = new ImageView(imagemBandeira);
+                            viewBandeira.setFitWidth(40);
+                            viewBandeira.setFitHeight(40);
+                            ((Button) node).setGraphic(viewBandeira);
                             continue;
                         }
                         if (mapa.mapa[i][j].isRevelado() == false){
-                            ((Button) node).setText(" ");
+                            ((Button) node).setGraphic(null);
+                            //((Button) node).setText(" ");
                             continue;
                         }
                         if (mapa.mapa[i][j].isRevelado()){
@@ -235,7 +254,15 @@ public class App extends Application{
                     int j = GridPane.getColumnIndex(node);
                     // if(mapa.mapa[i][j].isBomba()) ((Button) node).setText("X"); // vai mostrar os Xs
                     if (mapa.mapa[i][j].isRevelado()){
-                         ((Button) node).setText("" + ((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes());
+                        if (((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes() == 0) ((Button) node).setStyle("-fx-text-fill: black; -fx-font-size: 20px; -fx-background-color: lightgray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+                        else if (((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes() == 1) ((Button) node).setStyle("-fx-text-fill: blue; -fx-font-size: 20px; -fx-background-color: lightgray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+                        else if (((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes() == 2) ((Button) node).setStyle("-fx-text-fill: green; -fx-font-size: 20px; -fx-background-color: lightgray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+                        else if (((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes() == 3) ((Button) node).setStyle("-fx-text-fill: red; -fx-font-size: 20px; -fx-background-color: lightgray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+                        else if (((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes() == 4) ((Button) node).setStyle("-fx-text-fill: darkblue; -fx-font-size: 20px; -fx-background-color: lightgray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+                        else if (((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes() == 5) ((Button) node).setStyle("-fx-text-fill: brown; -fx-font-size: 20px; -fx-background-color: lightgray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+                        else if (((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes() == 6) ((Button) node).setStyle("-fx-text-fill: orange; -fx-font-size: 20px; -fx-background-color: lightgray; -fx-border-color: rgb(" + 100 + "," + 100 + ", " + 100 + ")");
+
+                        ((Button) node).setText("" + ((CampoSemBomba) mapa.mapa[i][j]).getBombasAdjacentes());
                     }
                 }
             }
